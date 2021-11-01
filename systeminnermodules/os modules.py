06a4a8c8c6f686f809c3024c6b340bb904bbaf67
HendricksJudy <61645034+HendricksJudy@users.noreply.github.com>
2021-11-01 23:08:26 +0800

    os modules

--- systeminnermodules/os modules.py
+++ systeminnermodules/os modules.py
@@ -30,3 +30,58 @@ print(path1)
 
 # os.makedirs(os.path.dirname(os.path.path)) 可以递归创建文件夹
 
+# os.rmdir() 删除 空（从未使用过） 文件夹
+
+# os.removedirs() 递归删除空文件夹
+
+# os.remove('/.DS_Store')  # 删除隐藏文件 即可删除 空文件夹
+
+# os.rename(原有名字， 目的名字) 修改文件或文件夹的名字
+
+# os。system() 执行操作系统中的命令
+
+# os.path 路径模块
+'''
+os.path.abspath()
+将相对路径转化为绝对路径 项目可移植
+
+获取路径中的主体部分
+os.path.basename()
+
+获取路径的路径部分 返回路径最后一部分之前的内容
+os.path.dirname()
+
+
+os.path.join(路径1， 路径2) 链接多个路径，组成一个新的路径
+
+os.path.split() 拆分路径，把路径分为路径和主体部分
+
+os.path.splitext() 拆分路径， 可以拆分文件后缀名
+
+获取文件大小 单位：字节
+os.path,getsize()
+
+检测是否为一个文件夹或是否存在
+os.path.isdir()
+
+# 检测文件是否存在
+os.path.isfile()
+
+# 检测路径是否存在 既可检测文件也可检测路径
+os.path.exist()
+
+# 检测两个path是否同时指向同一个目标位置 （两个路径必须真实）
+os.path.samefile(路径1, 路径2)
+
+# 检测该路径的最后某些时间
+os.path.getmtime()  # 修改
+os.path.getatime()  # 打开
+os.path.getctime()  # 创建
+
+
+
+
+'''
+
+
+
