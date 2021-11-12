837c0fc9f5bef78f58570b9dcbfaa492110754d3
HendricksJudy <61645034+HendricksJudy@users.noreply.github.com>
2021-11-12 21:04:59 +0800

    log_function

--- object_oriented_program/__del__ 析构方法.py
+++ object_oriented_program/__del__ 析构方法.py
@@ -9,18 +9,28 @@ __del__ 方法不带参数，但是必须要有返回值。
 __del__ 方法的返回值，是用来表示对象的状态，如果返回值是 True，
 那么对象的状态就是正常的，如果返回值是 False，那么对象的状态就是异常的。
 
+是销毁触发了析构
+
+对象在哪些情况下会被销毁？
+1. 当程序执行完毕，内存中所有资源都会被销毁释放
+2. 当对象被引用的次数为0时，对象被销毁
+3. 使用 del 删除时，对象被销毁
+
 '''
 
 import time
 
+from my_firstoop import xiaoMi
 
-class writelog():
+
+class WriteLog:
     # 成员属性
     fileurl = './'
     filename = time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'  # 日志文件名称
 
     # 初始化 打开文件
     def __init__(self):
+        print('初始化方法被触发了, 完成文件打开')
         self.f = open(self.fileurl + self.filename, 'a+', encoding='utf-8')
 
     # 日志写入
@@ -33,5 +43,67 @@ class writelog():
         # 在对象被销毁时，关闭在初始化方法中打开的文件对象的状态
         self.f.close()
 
-l = writelog()
-l.write_log('今天天气真不错，Github Copilot真好用')
+
+log_today = WriteLog()
+log_today.write_log('今天天气真不错，Github Copilot真好用')
+
+# 使用 del 手动删除
+del log_today
+'''
+
+未删除时顺序
+把日志：今天天气真不错，Github Copilot真好用 写入文件中
+....
+析构方法被触发了
+
+删除时的顺序
+把日志：今天天气真不错，Github Copilot真好用 写入文件中
+析构方法被触发了
+....
+
+'''
+
+print('....')
+
+# 当对象不再被引用时
+WriteLog()
+'''
+初始化方法被触发了, 完成文件打开
+析构方法被触发了
+'''
+
+class phone(object):
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        print(f'初始化方法被触发：创建{self.name}手机')
+
+    def __del__(self):
+        print(f'析构方法被触发：{self.name}手机被销毁了')
+
+# XiaoMi = phone('小米', '1000')
+# iphone = phone('iphone', '5000')
+# Samsung = phone('三星', '3000')
+'''
+以上三行的执行顺序是：
+初始化方法被触发：创建小米手机
+初始化方法被触发：创建iphone手机
+初始化方法被触发：创建三星手机
+析构方法被触发：小米手机被销毁了
+析构方法被触发：iphone手机被销毁了
+析构方法被触发：三星手机被销毁了
+'''
+
+phone('小米', '1000')
+phone('iphone', '5000')
+phone('三星', '3000')
+'''
+以上三行的执行顺序是：
+初始化方法被触发：创建小米手机
+析构方法被触发：小米手机被销毁了
+初始化方法被触发：创建iphone手机
+析构方法被触发：iphone手机被销毁了
+初始化方法被触发：创建三星手机
+析构方法被触发：三星手机被销毁了
+'''
+
---
+++ object_oriented_program/oop-logsubjectpackage.py
@@ -0,0 +1,53 @@
+# 日志类封装
+"""
+日志类
+     class      MyLog
+     功能：
+         1.记录日志
+         2.记录异常
+         3.记录报错
+     储存位置：默认在当前目录
+     日志文件名：默认为当前日期
+     日志格式：年月日时分秒 错误信息：（）
+
+     属性：存储信息供成员方法调用
+        储存地址：fileURL
+        日志文件名：filename
+        日志对象：logobj
+
+
+     方法：完成一个功能的过程
+        __init__ ==> 初始化方法，并打开文件
+        writeLog() ==> 接收日志信息，并写入到日志文件中
+        __del__ ==> 析构方法，在对象被销毁时，关闭打开的文件
+"""
+import time
+
+
+class MyLog():
+    # 属性
+    fileURL = './'
+    filename = time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.log'
+    logobj = None
+
+    # 方法
+    def __init__(self):
+        # 初始化方法，并打开文件
+        self.logobj = open(self.fileURL + self.filename, 'a+', encoding='utf-8')
+
+    def writeLog(self, msg):
+        # 准备写入信息
+        now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        logmsg = now + ' ' + msg + '\n'
+        # 显示准备写入的信息
+        print(logmsg)
+        # 写入信息
+        self.logobj.write(logmsg)
+
+    def __del__(self):
+        # 关闭打开的文件
+        self.logobj.close()
+
+
+todayrecord = MyLog()
+todayrecord.writeLog('今天也是用Copilot的一天！')
